1eb7a0fb7951c349b61327ee50c30f5da87e6873
Brandon Boone <BranAndJus@gmail.com>
2014-07-13 22:42:34 -0400

    Initial Checkin

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+﻿module.exports = function (grunt) {
+  
+    grunt.initConfig({
+        jshint: {
+            dev: {
+                files: {
+                    src: ['lib/**/*.js', 'app.js']
+                }
+            },
+            options: {
+                laxcomma: true,
+                ignores: ['lib/templates.js', 'Gruntfile.js']
+            }
+        },
+
+        handlebars: {
+            dev: {
+                files: {
+                    "lib/templates.js": ["templates/*.hbr"]
+                }
+            },
+            release: {
+                files: {
+                    "lib/templates.js": ["templates/*.hrb"]
+                }
+            },
+            options: {
+                commonjs:true
+            }
+        },
+        watch: {
+            files: ['**/*.js', 'templates/*.hbr', '!templates.js', '!Gruntfile.js'],
+            tasks: ['handlebars:dev', 'jshint:dev'],
+            options: {
+                livereload: true
+            }
+        }
+    });
+
+
+    [
+        , 'grunt-contrib-jshint'
+        , 'grunt-contrib-handlebars'
+        , 'grunt-contrib-watch'
+    ].forEach(function (item) {
+        grunt.loadNpmTasks(item);
+    });
+
+
+    var previous_force_state = grunt.option("force");
+
+    grunt.registerTask("force", function (set) {
+        if (set === "on") {
+            grunt.option("force", true);
+        }
+        else if (set === "off") {
+            grunt.option("force", false);
+        }
+        else if (set === "restore") {
+            grunt.option("force", previous_force_state);
+        }
+    });
+
+    // Default task(s).
+    grunt.registerTask('dev', ['handlebars:dev', 'jshint:dev']);
+    grunt.registerTask('release', ['handlebars:release']);
+
+
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+#autotask.js
+
+A library that wraps the [Autotask SOAL API](https://www.autotask.net/help/content/Userguides/T_WebServicesAPIv1_5.pdf). 
+
+##Console driver configuration
+
+#1 Install the node.js runtime: http://nodejs.org/
+#1 Install grunt-cli: (terminal/console) npm install -g grunt-cli
+#1 Clone the repositiory to your computer. 
+#1 Navigate to the autotask.js directory: (terminal/console) npm install
+#1 In the same directory: grunt dev
